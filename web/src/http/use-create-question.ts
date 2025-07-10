93a9c4b3f5d89cd3b4956ea0465b3d47436b0512
caio-ferreira-dev <caioferreirasdj@gmail.com>
2025-07-10 11:37:10 -0300

    feat: add create question integration, record room audio page

---
+++ web/src/http/use-create-question.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { CreateRoomResponse } from "./types/create-room-response";
+import type { CreateQuestionRequest } from "./types/create-question-request";
+
+export function useCreateQuestion(roomId: string) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (data: CreateQuestionRequest) => {
+      const response = await fetch(
+        `http://localhost:3333/rooms/${roomId}/question`,
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      const result: CreateRoomResponse = await response.json();
+
+      return result;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["get-rooms", roomId] });
+    },
+  });
+}
